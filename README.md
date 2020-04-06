9e6012e14fb790606624ddc634774ceffd91d2f6
noipzjffls <ericoder@qq.com>
2020-04-06 14:32:47 +0800

    Created the repository of GENERATOR! Hello world!

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# generator
-A program used to generate random sequences including letters and numbers.
+# Generator
+A program used to generate random sequences including letters and numbers. It is written in C/C++.
+For users of Unix (including Linux and MacOS), first download a file:
+[Edition 1 (output in terminal or other systems)](/UNIX/GENERATOR1.0.c)
+[Edition 2 (output could be externaled in another output file)](/UNIX/GENERATOR2.0.cpp)
+How do use in Unix:
+You can build and run the generators neither in the terminal or in an application that can run C/C++, for example Visual Studio Code. If you run them in an application, do as how you run proper C/C++ programs. If you would like to run them in the terminal, first prepare the terminal and go to the directory and follow these steps:
+## Version 1.0
+```bash
+gcc -o generator1.0 generator1.0.c
+./generator1.0
+```
+## Version 2.0
+```bash
+g++ -o generator2.0 generator2.0.cpp
+./generator2.0
+```
+For users of Windows, first download a file:
+[Edition 1 (output in terminal or other systems)](/WINDOWS/GENERATOR1.0.c)
+[Edition 2 (output could be externaled in another output file)](/WINDOWS/GENERATOR2.0.cpp)
+How do use in Windows:
+You can build and run the generators neither in the command prompt (cmd.exe) or in an application that can run C/C++, for example Visual Studio Code. If you run them in an application, do as how you run proper C/C++ programs. If you would like to run them in the cmd.exe, first prepare the cmd.exe by right clicking the Windows icon and entering the cmd.exe, then go to the path of the generator.
+## Version 1.0
+```bash
+gcc -o generator1.0.exe generator1.0.c
+start generator1.0.exe
+```
+## Version 2.0
+```bash
+g++ -o generator2.0.exe generator2.0.cpp
+start generator2.0.exe
+```
+F.A.Q:
+Q: Why does the cmd (terminal) respond error: no such file or repository?
+A: You might be entering the wrong path. To correct this, run:
+```bash
+cd /
+cd [path]
+```
+The \[path] above is equivalent to the path where you downloaded the generator.
+Next, redo the work that is shown above.
---
+++ UNIX/GENERATOR2.0.cpp
@@ -0,0 +1,88 @@
+#include<bits/stdc++.h>
+#include<unistd.h>
+using namespace std;
+#define random(a,b)rand()%(b-a+1)+a
+char ch;
+int x,t,pos;
+string filename,a,b;
+int find(string s)
+{
+    for(int i=0;s[i];i++)if(s[i]=='.')return i;
+    return -1;
+}
+void generate(int len,int i)
+{       
+    if(filename!="NO")
+    {
+        stringstream ss;
+        string num,N;
+        char fname[1001];
+        if(t==1)
+        {
+            ss<<filename;
+            ss>>fname;
+            freopen(fname,"w",stdout);
+        }
+        else
+        {
+            ss<<i;
+            ss>>N;
+            num=filename;
+            num.insert(pos,N);
+            ss.clear();
+            ss<<num;
+            ss>>fname;
+            freopen(fname,"w",stdout);
+        }
+    }
+    while(len--)
+    {
+        int n=random(0,2);
+        switch(n)
+        {
+            case 0:
+            {
+                putchar(random(48,57));
+                break;
+            }
+            case 1:
+            {
+                putchar(random(65,90));
+                break;
+            }
+            case 2:
+            {
+                putchar(random(97,122));
+                break;
+            }
+        }
+    }
+    putchar('\n');
+}
+int main()
+{
+    srand(time(NULL));
+    cout<<"You will now generate a number + letter sequence.\nType ENTER/RETURN to continue or press any other key then press ENTER/RETURN to abort.";
+    ch=getchar();
+    if(ch=='\n')
+    {
+        puts("How many sequences do you want?");
+        scanf("%d",&t);
+        puts("How many digits do you want?");
+        scanf("%d",&x);
+        puts("Would you like to write the sequences into a file? Type \"NO\" if you don't want to or type the file name to write into a file. For example, a valid file name is \"data.out\". The data will be generated in \"data1.out,data2.out,...\". If you need only 1 sequence, then the sequence will be written into the filename you have given.");
+        cin>>filename;
+        pos=find(filename);
+        if(pos==-1&&filename!="NO")
+        {
+            puts("File name format ERROR!");
+            return 0;
+        }
+        puts("Generating, please wait...");
+        a=filename.substr(0,pos);
+        b=filename.substr(pos+1);
+        sleep(1);
+        for(int i=1;i<=t;i++)generate(x,i);
+    }
+    else puts("Program aborted successfully. Have a nice day.");
+}
